d03b4de74b9750f7ad5c55a98d6121ec2141729d
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-03-04 01:19:15 +0000

    [add]ルーティング追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
 
   def top
-  def
+  end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'spots/new'
-  get 'spots/index'
-  get 'spots/show'
-  get 'spots/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+
   devise_for :users
+
+  root to:"homes#top"
+
+  resources :users, only:[:show, :edit, :update, :destroy] do
+    member do
+      get 'mypage'
+    end
+  end
+
+  resources :books do
+    resources:comments, controller: 'books/comments', only:[:create, :destroy]
+    resource:bookmarks, controller: 'books/bookmarks', only:[:create, :destroy]
+  end
+
+  resources :spots do
+    resources:comments, controller: 'spots/comments', only:[:create, :destroy]
+    resource:went, controller: 'spots/wents',only:[:create, :destroy]
+    resource:want_to_gos, controller: 'spots/want_to_gos', only:[:create, :destroy]
+  end  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
