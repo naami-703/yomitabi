e36b9e09bb7a2f5b4b87ebb78c2538ea997207d6
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-03-07 03:14:50 +0000

    [update]メニューをサイドバー→ヘッダー下段に変更

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,6 +8,10 @@ class Public::UsersController < ApplicationController
     @spots = @user.spots
   end
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to:"public/homes#top"
   get 'about', to: 'public/homes#about'
 
-  resources :users, controller: 'public/users', only:[:show, :edit, :update, :destroy] do
+  resources :users, controller: 'public/users', only:[:index, :show, :edit, :update, :destroy] do
     collection do
       get 'mypage'
     end
