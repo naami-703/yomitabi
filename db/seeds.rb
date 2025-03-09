993fac3c0d586fc4d0ebca357247dc57f415485c
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-03-09 05:12:42 +0000

    [update]seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+山田太郎 = User.find_or_create_by!(email: "test@test.co.jp") do |user|
+  user.name = "山田太郎"
+  user.password = "password"
+  user.introduction = "こんにちは"
+  user.favorite_books = "ワンピース"
+end
+
+佐藤花子 = User.find_or_create_by!(email: "test2@test.co.jp") do |user|
+  user.name = "佐藤花子"
+  user.password = "password"
+  user.introduction = "よろしく"
+  user.favorite_books = "十二国記"
+end
+
+Book.find_or_create_by!(title: "四畳半神話大系") do |book|
+  book.user_id = 山田太郎.id
+end
+
+Book.find_or_create_by!(title: "坂道のアポロン") do |book|
+  book.user_id = 佐藤花子.id
+end
+
+Spot.find_or_create_by!(name: "鴨川デルタ") do |spot|
+  spot.user_id = 山田太郎.id
+  spot.address = "京都府京都市左京区下鴨宮河町"
+end
+
+Spot.find_or_create_by!(name: "三浦町カトリック教会") do |spot|
+  spot.user_id = 佐藤花子.id
+  spot.address = "長崎県佐世保市三浦町４−２５"
+end
