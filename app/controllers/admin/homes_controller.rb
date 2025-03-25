96007b2f06128aca7e0f1e9449551a434213fbd2
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-03-25 01:21:26 +0000

    [update]admin/top ページネーション調整

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,9 +2,11 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top 
-    @books = Book.page(params[:page])
+    books = Book.all
+    @books = books.order(created_at: :desc).page(params[:page]).per(12)
     @books_new = @books.order(created_at: :desc).limit(4)
-    @spots = Spot.page(params[:page])
+    spots = Spot.all
+    @spots = spots.order(created_at: :desc).page(params[:page]).per(12)
     @spots_new = @spots.order(created_at: :desc).limit(4)
   end
   
