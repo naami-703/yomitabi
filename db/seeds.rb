54e62926a6966ca4cf7bdf2fb2d2d33ecbe60d0c
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-03-25 04:45:00 +0000

    [finish]フェーズ3実装完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,25 +8,18 @@
 
 山田太郎 = User.find_or_create_by!(email: "test@test.co.jp") do |user|
   user.name = "山田太郎"
-  user.password = "password"
+  user.password = "111111"
   user.introduction = "こんにちは"
-  user.favorite_books = "ワンピース"
+  user.favorite_books = "ミステリー小説全般"
 end
 
 佐藤花子 = User.find_or_create_by!(email: "test2@test.co.jp") do |user|
-  user.name = "佐藤花子"
-  user.password = "password"
-  user.introduction = "よろしく"
-  user.favorite_books = "十二国記"
+  user.name = "小林花子"
+  user.password = "222222"
+  user.introduction = "よろしくお願いします！"
+  user.favorite_books = "ベルサイユのばら"
 end
 
 Admin.find_or_create_by!(email: "admin@admin.co.jp") do |admin|
-  admin.password = "password"
+  admin.password = "000000"
 end
-
-コミック = Genre.find_or_create_by!(name: "コミック")
-小説 = Genre.find_or_create_by!(name: "小説")
-
-
-
-
