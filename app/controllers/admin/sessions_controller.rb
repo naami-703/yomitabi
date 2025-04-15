68009202b61e693905907e8cc188ddf2e83284ff
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-04-15 06:44:39 +0000

    [update]admin/header 不具合修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,6 +3,9 @@
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  before_action :set_genres
+  before_action :set_regions
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -38,6 +41,16 @@ class Admin::SessionsController < Devise::SessionsController
 
   def after_sign_out_path_for(resource)
     new_admin_session_path
+  end
+
+  private
+
+  def set_genres
+    @genres = Genre.all
+  end
+
+  def set_regions
+    @all_region_names = Location.distinct.pluck(:name)
   end
   
 end
